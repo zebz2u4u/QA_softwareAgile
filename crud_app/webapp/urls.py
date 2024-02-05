76d007f96516d8b07618253efaae6b5fe622fe9a
Deborah <debie02awoyemi@gmail.com>
2024-02-05 17:54:36 +0000

    Registartion and Login
    - Add login and registration forms
    - connect links on homepage, login form, and registration form
    
    Signed-by <debie02awoyemi@gmail.com>

--- crud_app/webapp/urls.py
+++ crud_app/webapp/urls.py
@@ -3,4 +3,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.homePage, name=""),
+
+    path('register', views.register, name="register"),
+    
+    path('login', views.login, name="login"),
 ]
