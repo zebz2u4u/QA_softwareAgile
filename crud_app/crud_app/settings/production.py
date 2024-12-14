97b7d44c8183e42710accd2ca4ab2c6032def84f
Deborah <debie02awoyemi@gmail.com>
2024-12-14 21:18:29 +0000

    changed debug settings

--- crud_app/crud_app/settings/production.py
+++ crud_app/crud_app/settings/production.py
@@ -25,7 +25,7 @@ from crud_app.settings import get_secret
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = ['*']
 
